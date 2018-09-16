11f87b4f18475ac256527ca9ce4239e87c41a39f
mlaakso@dev <laakso.mavrick@gmail.com>
2018-09-16 01:05:23 -0400

    team issue counts

--- resources/js/components/team-header.component.vue
+++ resources/js/components/team-header.component.vue
@@ -1,27 +1,46 @@
 <template>
-    <div class="w-screen h-24 bg-white">
+    <div class="w-screen h-auto bg-white">
         <div class="team-container container mx-auto p-4">
             <h1>
                 {{ teamName }}
             </h1>
+            <div class="team-issues mt-4">
+                <team-issue class="left-aligned" header="Backlog" :count="teamTodoCount"></team-issue>
+                <team-issue header="In Progress" :count="teamInProgressCount"></team-issue>
+                <team-issue header="Done" :count="teamDoneCount"></team-issue>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import { mapState } from 'vuex';
+import TeamIssue from './team-issue.component';
 
 export default {
 
-    computed: {
-        teamName: function () {
-            const team = this.getSelectedTeam;
+    components: {
+        TeamIssue
+    },
+
+    computed: mapState({
+        teamName: function (state) {
+            const team = state.teams.selectedTeam;
             return team ? team.name : '';
         },
-        ...mapGetters([
-            'getSelectedTeam'
-        ])
-    }
+        teamTodoCount: function (state) {
+            const team = state.teams.selectedTeam;
+            return team ? team.todo_issues_count : 0;
+        },
+        teamInProgressCount: function (state) {
+            const team = state.teams.selectedTeam;
+            return team ? team.in_progress_issues_count : 0;
+        },
+        teamDoneCount: function (state) {
+            const team = state.teams.selectedTeam;
+            return team ? team.done_issues_count : 0;
+        }
+    })
 
 }
 </script>
@@ -29,5 +48,14 @@ export default {
 @import "~@/app.scss";
 .team-container {
   max-width: $max-width;
+  display: flex;
+  flex-direction: column;
+}
+.team-issues {
+  display: flex;
+}
+.left-aligned {
+  margin-left: 0 !important;
+  padding-left: 0 !important;
 }
 </style>
