28d558b1a2cbab94a5cd0003b92f5250ceafeded
mlaakso@dev <laakso.mavrick@gmail.com>
2018-09-18 22:14:13 -0400

    icons

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,6 +4,9 @@ import App from "./layouts/app.layout";
 import router from "./app.routes";
 import store from "./store/app.store";
 
+//import "vue-material-design-icons/styles.css";
+import "vue-awesome/icons/plus-circle";
+
 sync(store, router);
 
 new Vue({
--- resources/js/components/create-project.component.vue
+++ resources/js/components/create-project.component.vue
@@ -1,15 +1,28 @@
 <template>
-    <div class="create-project p-4 m-2 w-auto h-48 rounded-lg border border-grey border-dashed"></div>
+    <div class="create-project p-4 m-2 w-auto h-48 rounded-lg border border-grey border-dashed">
+        <icon name="plus-circle" :scale="2.5" />
+    </div>
 </template>
 
 <script>
+import Icon from 'vue-awesome/components/Icon'
+export default {
+    components: {
+        Icon
+    }
+}
 </script>
 
-<style>
+<style lang="scss" scoped>
 .create-project {
   display: flex;
   justify-content: center;
   align-items: center;
   cursor: pointer;
+  color: #b8c2cc; //text-grey
+  transition: color 100ms;
+}
+.create-project:hover {
+  color: #8795a1; //text-grey-light
 }
 </style>
